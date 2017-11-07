6a0e80b10d681b8bb0568333619799e7aa3fe2fc
Yomar Miranda <mirandayomar@gmail.com>
2017-11-06 20:42:47 -0500

    Como podemos ver, en esta ruta se van a guardar todas la vistas de nuestra aplicación, si apreciamos mejor podemos observar que esta vista no se llama index debido a que en la rutas se realiza un configuración para cuando se acceda a la raiz del proyecto se llame a esta vista en especifico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Aprendiendo Laravel
                 </div>
 
                 <div class="links">
