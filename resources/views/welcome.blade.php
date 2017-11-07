8bb8c92572a4583906325411a26843dc658d6435
Yomar Miranda <mirandayomar@gmail.com>
2017-11-06 21:23:52 -0500

    En las vistas podemos como utilizar sintaxis  Blade-PHP, en este caso realizamos un ciclo 'foreach' sobre un array que pasamos desde las rutas, cabe resaltar que para pasar variables a las vistas desde la rutas lo debemos hacer desde un Array, en este caso pasamos un array pero también podemos pasar otro tipo de variables y trabajar con ellas en las vistas. Nota: Las instrucciones especiales se les anteponde un 'arroba' y para mostras variables debemos encerrarlas dentro de 'dobles llaves'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @foreach ($links as $link => $text)
+                        <a href="{{ $link }}">{{ $text }}</a>
+                    @endforeach
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	$links = [
+		"https://yomar-dev.github.io/blog/" => "Blog",
+        "https://developer.mozilla.org/es/profiles/yomar-dev/" => "MDN",
+        "https://gitlab.com/yomar_dev" => "GitLab",
+        "https://twitter.com/yomar3092" => "Twitter",
+        "https://github.com/yomar-dev" => "GitHub"
+	];
+
+    return view('welcome', [
+    	"links" => $links
+    ]);
 });
 
 
