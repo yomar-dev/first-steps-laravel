5384e505ae0d175ea6bd389c90c3cd63f662d1b5
Yomar Miranda <mirandayomar@gmail.com>
2017-11-11 23:58:33 -0500

    Creamos el metodo show el cual nos va a servir para mostrar un determinado show recibiendo como parametro el ID del mensaje. Hay que tener el cuenta que al momento de retonar una vista le hemos dicho 'messages.show' Laravel lo interpreta como si 'messages' es una carpeta y 'show' como una plantilla de Blade

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    //
+    public function show($id){
+    	$message = Message::find($id);
+
+    	return view('messages.show', [
+    		'message' => $message
+    	])
+    }
 }
