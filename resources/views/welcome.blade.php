7c7c6151286e9d07e057ac506d2296e8d7bcbd6d
Yomar Miranda <mirandayomar@gmail.com>
2017-11-12 00:27:46 -0500

    Creamos un formulario en el Home el cual nos va servir para crear mensajes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,14 @@
     </div>
 
     <div class="row">
+        <form action="/message/create" method="post">
+            <div class="form-group">
+                <input type="text" name="message" class="form-control" placeholder="Que estás pensando?">
+            </div>
+        </form>
+    </div>
+
+    <div class="row">
         @forelse ($messages as $message)
             <div class="col-6">
                 <img class="img-thumbnail" src="{{ $message->image }}" alt="">
