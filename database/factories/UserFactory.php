72cae50fbf7a14768dbe8e4ef6ce0360b54e29bb
Yomar Miranda <mirandayomar@gmail.com>
2017-11-12 22:31:48 -0500

    Esta función de Faker puede recibir un parametro el cual indica cuantos caracteres queremos contenga nuestro texto, pero como no queremos que todos tengas la misma cantidad de caracteres le hemos pasada un número aleatorio entre 20 y 160 para que nuestros mensajes parezcan más realistas. Para corregir el error anterior en el cual Faker nos generó texto de 200 caracteres ejecutados un Migrate para rehacer todo nuevamente, ya que de esta manera nos eliminará todo el contenido existente en la BD y nos creará los 100 registros que le hemos pedido, todo eso la hace al ejecutar el siguiente comando: 'php artisan db:seed'

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ $factory->define(App\User::class, function (Faker $faker) {
 
 $factory->define(App\Message::class, function (Faker $faker){
 	return [
-		'content' => $faker->realText(),
+		'content' => $faker->realText(random_int(20, 160)),
 		'image' => $faker->imageUrl(600, 338)
 	];
 });
