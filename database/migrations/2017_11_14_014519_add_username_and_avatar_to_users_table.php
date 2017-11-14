dd629044123e7038e28be2359a83dcae706a440a
Yomar Miranda <mirandayomar@gmail.com>
2017-11-13 20:58:53 -0500

    Hemos alterado la migration creada anteriormente para poder agregar las columnas que necesitamos en nuestra tabla se usuarios.

--- database/migrations/2017_11_14_014519_add_username_and_avatar_to_users_table.php
+++ database/migrations/2017_11_14_014519_add_username_and_avatar_to_users_table.php
@@ -14,7 +14,8 @@ class AddUsernameAndAvatarToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('username')->unique();
+            $table->string('avatar')->nullable();
         });
     }
 
@@ -26,7 +27,8 @@ class AddUsernameAndAvatarToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('username');
+            $table->dropColumn('avatar');
         });
     }
 }
